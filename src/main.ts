0e3f10a613e258fe3934098bae6f8313544dda56
Weslei Almeida Muniz <88255036+WesleiAlmeidaMuniz@users.noreply.github.com>
2024-10-23 22:13:10 -0300

    Criação da docker e TypeORM

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
+require('dotenv').config();
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
